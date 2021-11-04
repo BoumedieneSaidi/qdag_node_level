74ef7c7e88b0d89d639d9e4f75e62d28c05ff8f0
Saidi Boumediene <saidiboumediene98@gmail.com>
2021-11-04 13:57:08 +0100

    some changes

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,104 @@
 var express = require('express');
 var router = express.Router();
-var sess;
-/* GET home page. */
+var userSession;
+
+/* GET home page. To remove after*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/demo', function(req, res, next) {
-  if(sess === undefined){
-      sess = req.session;
-      sess["queries"] = [];
+
+/***************************** Utils ***************************/
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+/** Get user Session queries With Results return "['query1','query2' ...]" */
+function init(req){
+  if(userSession === undefined){
+    userSession = req.session;
+    userSession["recentRdfQueries"] = new Map();
+    userSession["recentQDAGQueries"] = new Map();
+    userSession["recentRequests"] = new Set();
+    //That would store all the recent queries with their results
+    userSession["queriesWithResults"] = new Set();
+    //the key index of the query
+    userSession["nextQueryIndex"] = 1;
   }
-  res.send({ queries: sess["queries"]});
+}
+function setResultParameters(initialQueryConfig,exectTime,nbrResult,result){
+    initialQueryConfig["execTimeQDAG"] = exectTime;
+    initialQueryConfig["nbrResult"] = nbrResult;
+    initialQueryConfig["result"] = result; 
+}
+function executeQDAGQuery(initialQueryConfig){
+  const qdagParameters = JSON.stringify({currentDB:initialQueryConfig["currentDB"],query:initialQueryConfig["query"],
+                        optimizer:initialQueryConfig["optimizer"],isElag:initialQueryConfig["isElag"],isSpatial:initialQueryConfig["isSpatial"],
+                        spatialStrategy:initialQueryConfig["spatialStrategy"]});
+  if(userSession["recentQDAGQueries"].has(qdagParameters)){
+    console.log("After 3amora",userSession["recentQDAGQueries"].get(qdagParameters));
+      const result = userSession["recentQDAGQueries"].get(qdagParameters);
+      setResultParameters(initialQueryConfig,result["execTimeQDAG"],result["nbrResult"], result["result"]);
+  } else {
+      setResultParameters(initialQueryConfig,getRandomArbitrary(1000,5000),4000,"<mancity>\n</pogba>");
+      userSession["recentQDAGQueries"].set(qdagParameters,{execTimeQDAG:initialQueryConfig["execTimeQDAG"],nbrResult:initialQueryConfig["nbrResult"],
+                                                                result:initialQueryConfig["result"]});
+      console.log("After store",userSession["recentQDAGQueries"]);
+  }
+}
+function executeRDFQuery(initialQueryConfig){
+  const rdfParameters = JSON.stringify({currentDB:initialQueryConfig["currentDB"],query:initialQueryConfig["query"]});
+  //check if rdf3x [currentDb,query] has been already executed
+  if(userSession["recentRdfQueries"].has(rdfParameters))
+      initialQueryConfig["execTimeRDF"] = userSession["recentRdfQueries"].get(rdfParameters);
+  else {
+      initialQueryConfig["execTimeRDF"] = getRandomArbitrary(1000,5000);
+      userSession["recentRdfQueries"].set(rdfParameters,initialQueryConfig["execTimeRDF"]);
+  }
+}
+//Parse the queriesWith results string map to object array of queries
+function parseQueriesWithResults(){
+    return [...userSession["queriesWithResults"]].map((stringObject) => JSON.parse(stringObject))
+}
+function hasHTTPRequested(strQuery){
+  return userSession["recentRequests"].has(strQuery);
+}
+function addStrQueryToRecentRequests(initialQueryConfig, strQuery){
+  userSession["recentRequests"].add(strQuery);
+  initialQueryConfig["key"] = userSession["nextQueryIndex"];
+  userSession["nextQueryIndex"] += 1;
+}
+function fetchResult(initialQueryConfig){
+  //1- first step : execute QDAG Query
+  executeQDAGQuery(initialQueryConfig);
+  //2- second step: execute RDF-3X QUERY
+  if(initialQueryConfig['rdfToo'] === "true")
+        executeRDFQuery(initialQueryConfig);
+  // update user Queries
+  userSession["queriesWithResults"].add(JSON.stringify(initialQueryConfig));
+}
+/************************************************************************/
+/************************************ Routes  ***************************/
+router.get('/demo', function(req, res, next) {
+  init(req);
+  res.send({"queriesWithResults": parseQueriesWithResults()});
 });
+/** Run Query generate with a fixed result (waiting to add QDAG) */
 router.get('/run-query', function(req, res, next) {
-  sess["queries"].push("Alpha")
-  /*userSession.queries.push(req.query)
-  console.log(req.query);*/
-  res.send({ 
-       exec_time: 'Express',
-       result:"Wait until addign QDAG to the process" 
-  });
+  init(req);
+  //Get the query parameters from the http request
+  let initialQueryConfig = req.query;
+  const strQuery = JSON.stringify(initialQueryConfig);
+  //if the request has been already sent => it's been already in the bar chart
+  if(hasHTTPRequested(strQuery)){
+      console.log("Hihiiiiiiiiiiii");
+      res.send({  "userQuery":{}  });
+  }
+  else {
+      addStrQueryToRecentRequests(initialQueryConfig,strQuery);
+      fetchResult(initialQueryConfig);
+      res.send({ "userQuery":initialQueryConfig });
+  }
 });
+/************************************************************************** */
+
+
 module.exports = router;
